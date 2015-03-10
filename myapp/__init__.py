9dde12f2998d14fe265fe7985f9e9cef477d2830
Brendan <b.emery94@gmail.com>
2015-03-10 17:51:44 +1100

    Added file logging for errors (when not in debugging mode)

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -4,3 +4,16 @@ from . import config
 app = Flask("myapp")
 app.config.from_object(config)
 from myapp import views
+
+# For error logging when debugging is False
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/{}.log'.format(app.config['APP_NAME']),
+                                       'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: '
+'%(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('{} startup'.format(app.config['APP_NAME']))
--- myapp/views.py
+++ myapp/views.py
@@ -10,3 +10,15 @@ def home_page():
 @app.route('/index')
 def index_page():
     return render_template('index.html', title='Index', app_name=app_name)
+
+# When debugging is set to off, these pages will show when the respective errors
+# are raised
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html', title='Error 404', app_name=app_name), \
+           404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html', title='Error 500', app_name=app_name), \
+           500

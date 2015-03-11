1ecb775cfbc6a3e998c507b45106d9b584867bbc
mra1385 <mikeazar@gmail.com>
2015-03-11 12:55:32 -0400

    Conformed variable names.

--- myapp/GMaps.py
+++ myapp/GMaps.py
@@ -3,8 +3,8 @@ __author__ = 'Mike Azar'
 from instance.config import Google_API
 import requests
 
-def get_coordinates(StNum,StName,StType,City, State):
-    r = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + StNum + '+' + StName + '+' + StType + ',+' + City + ',+' + State + '&sensor=FALSE' + Google_API)
+def get_coordinates(st_num, st_name, st_type, city, state):
+    r = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + st_num + '+' + st_name + '+' + st_type + ',+' + city + ',+' + state + '&sensor=FALSE' + Google_API)
     data = r.json()
     latitude = data['results'][0]['geometry']['location']['lat']
     longitude = data['results'][0]['geometry']['location']['lng']

61cad8a60cdddae06621d2dd39a7891c2b951b0e
Brendan Emery <b.emery94@gmail.com>
2015-03-11 19:26:35 +1100

    Put error handling views in their own module and error templates in their own directory. Added a registration form with data validation and corresponding html file and views function

--- myapp/config.py
+++ myapp/config.py
@@ -1,2 +1,6 @@
 APP_NAME = 'Social Proximity Web App'
 CHOSEN_MEDIA = ['Twitter', 'Instagram', 'Yelp', 'Facebook']
+
+# Security for login/registration pages.
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'

e59571d64480bcc433a4aca30b9aa83a30dc062e
Brendan <b.emery94@gmail.com>
2015-03-11 14:19:25 +1100

    Added function to add pages and links to a dict. Link at top of page are automatically made from this dict. Also added basic login page

--- myapp/views.py
+++ myapp/views.py
@@ -1,28 +1,36 @@
 from myapp import app
 from flask import render_template
+import collections
 
 app_name = app.config['APP_NAME']
 chosen_media = app.config['CHOSEN_MEDIA']
 
-# Add the name and link for each page we make to this. They will automatically
-# be added to the links at the top. Need to find a way to order them
-page_dict = {}
+def setup_page_dict():
+    """Make a dictionary of all the pages in the file for links at the top of
+    the web page. Add the name and address of every new page here"""
+    page_dict = collections.OrderedDict()
+    page_dict['Home'] = '/'
+    page_dict['Index'] = '/index'
+    page_dict['Login'] = 'Login'
+    return page_dict
 
 @app.route('/')
 def home_page():
-    page_dict['Home'] = '/'
-    return render_template('home.html', title='Home', page_dict=page_dict,
+    return render_template('home.html', title='Home',
+                           page_dict=setup_page_dict(),
                            app_name=app_name)
 
 @app.route('/index')
 def index_page():
-    page_dict['Index'] = '/index'
-    return render_template('index.html', title='Index', page_dict=page_dict,
+    return render_template('index.html', title='Index',
+                           page_dict=setup_page_dict(),
                            chosen_media=chosen_media)
 
-# @app.route('/Login')
-# def login():
-#     return
+@app.route('/Login')
+def login():
+    return render_template('login.html', title='Login',
+                           page_dict=setup_page_dict(),
+                           chosen_media=chosen_media)
 
 # When debugging is set to off, these pages will show when the respective errors
 # are raised

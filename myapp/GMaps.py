4df994d0c3b5c6d447063d63d87b773ce2aebe51
mra1385 <mikeazar@gmail.com>
2015-03-11 10:02:15 -0400

    Added Google Maps API, started Twitter API, added API keys

--- myapp/GMaps.py
+++ myapp/GMaps.py
@@ -1,11 +1,9 @@
 __author__ = 'Mike Azar'
 
-from config import API
-import requests
-import json
+from instance.config import Google_API
 
 def get_coordinates(StNum,StName,StType,City, State):
-    r = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + StNum + '+' + StName + '+' + StType + ',+' + City + ',+' + State + '&sensor=FALSE' + API)
+    r = requests.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + StNum + '+' + StName + '+' + StType + ',+' + City + ',+' + State + '&sensor=FALSE' + Google_API)
     data = r.json()
     latitude = data['results'][0]['geometry']['location']['lat']
     longitude = data['results'][0]['geometry']['location']['lng']

d82029f85e56fdf9cb8113d1673173ad7d2a1ae2
mra1385 <mikeazar@gmail.com>
2015-03-12 10:37:14 -0400

    Added Google Maps API test case as a class. Added unittest module to requirements.txt

--- Unittests/Unittests.py
+++ Unittests/Unittests.py
@@ -1,4 +1,10 @@
 from Glocal.API import GMaps
+import unittest
 
-#to convert below into conforming test case
-assert GMaps.get_coordinates('1500','Massachusetts','Avenue','Washington','DC') == (38.9064936, -77.03541179999999)
+class TestGMapsAPI(unittest.TestCase):
+
+    def test_get_coordinates(self):
+        self.assertEqual((GMaps.get_coordinates('1500','Massachusetts','Avenue','Washington','DC')), (38.9064936, -77.03541179999999))
+
+if __name__ == '__main__':
+    unittest.main()

572eadf055b10687592d8db35838454109ae321d
mra1385 <mikeazar@gmail.com>
2015-03-11 21:53:49 -0400

    Fixed unittest. To do: convert unittest into a Class for consistency

--- Unittests/Google Maps API.py
+++ Unittests/Google Maps API.py
@@ -1,7 +1,4 @@
 __author__ = 'Mike Azar'
 from myapp import GMaps
 
-assert GMaps.get_coordinates('1500','Massachusetts','Avenue','Washington','DC')[0] == 38.9064936
-
-# assert GMaps.get_coordinates('1500','Massachusetts','Avenue','Washington','DC')[1] == -77.0354118
-# This assert does not work. Unsure why that is.
+assert get_coordinates('1500','Massachusetts','Avenue','Washington','DC') == (38.9064936, -77.03541179999999)

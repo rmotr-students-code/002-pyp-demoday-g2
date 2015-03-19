9c1d047a0d0c41caae8d199669bb2f3abe46b5b3
mra1385 <mikeazar@gmail.com>
2015-03-19 12:20:49 -0400

    Added media url to tweets table. Modified unittests based on Paulos suggestions.

--- Unittests/Unittests.py
+++ Unittests/Unittests.py
@@ -1,45 +1,26 @@
 from Glocal.API import API
 import unittest
 
-class TestGetCoordinates(unittest.TestCase):
+class TestAPI(unittest.TestCase):
     def setUp(self):
         self.st_num = "1500"
         self.st_name = "Massachusetts"
         self.st_type = "Avenue"
         self.city = "Washington"
         self.state = "DC"
-        user_query = API.GlocalAPI(self.st_num, self.st_name, self.st_type,
-                                   self.city, self.state)
-        self.latitude, self.longitude = user_query.get_coordinates()
+        self.user_query = API.GlocalAPI(self.st_num, self.st_name, self.st_type,
+                                        self.city, self.state)
+        self.latitude, self.longitude = self.user_query.get_coordinates()
 
     def test_get_coordinates(self):
         self.assertEqual((self.latitude,self.longitude),
                          (38.9064936, -77.03541179999999))
 
-class TestGetTweets(unittest.TestCase):
-    def setUp(self):
-        self.st_num = "1500"
-        self.st_name = "Massachusetts"
-        self.st_type = "Avenue"
-        self.city = "Washington"
-        self.state = "DC"
-        self.user_query = API.GlocalAPI(self.st_num, self.st_name, self.st_type,
-                                        self.city, self.state)
     def test_get_tweets(self):
         self.assertIsNotNone(self.user_query.get_tweets())
 
     def test2_local_tweets(self):
         self.assertTrue(len(self.user_query.get_tweets()) > 1)
-
-class TestGetInstagram(unittest.TestCase):
-    def setUp(self):
-        self.st_num = "1500"
-        self.st_name = "Massachusetts"
-        self.st_type = "Avenue"
-        self.city = "Washington"
-        self.state = "DC"
-        self.user_query = API.GlocalAPI(self.st_num, self.st_name, self.st_type,
-                                        self.city, self.state)
 
     def test_get_instagram(self):
         self.assertIsNotNone(self.user_query.get_instagram())

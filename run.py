4f5e39821fc4e7cdf2386c0541f32da579ea5bb5
mra1385 <mikeazar@gmail.com>
2015-03-24 20:00:15 -0400

    added port to procfile

--- run.py
+++ run.py
@@ -2,6 +2,10 @@
 # directories
 
 from Glocal import app
+import os
+
+port = int(os.environ.get("PORT", 33507))
+
 if __name__ == "__main__":
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=port)
